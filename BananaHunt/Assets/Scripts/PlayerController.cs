12cf7ef3ff6a97c113170ebbf3bb763f05dd563d
gudenamrutha <gudenamrutha@gmail.com>
2024-09-28 10:40:11 +0530

    Player Movement Completed

---
+++ BananaHunt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Animator playerAnimations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Left Button Pressed");
+            StartCoroutine(JumpLeft());
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Right Button Pressed");
+            StartCoroutine(JumpRight());
+        }
+
+        //playerAnimations.SetBool("jump_left", false);
+        //playerAnimations.SetBool("jump_right", false);
+    }
+    IEnumerator JumpLeft()
+    {
+        playerAnimations.SetBool("jump_left", true);
+        yield return new WaitForSeconds(1);
+        playerAnimations.SetBool("jump_left", false);
+    }
+
+    IEnumerator JumpRight() {
+        playerAnimations.SetBool("jump_right", true);
+        yield return new WaitForSeconds(0.5f);
+        transform.rotation = Quaternion.Euler(0f, 0f, 45f);
+        playerAnimations.SetBool("jump_right", false);
+
+    }
+}
