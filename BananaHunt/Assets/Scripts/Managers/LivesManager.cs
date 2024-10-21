7a91b10abb106ec7ae07423688b27e012be30384
gudenamrutha <gudenamrutha@gmail.com>
2024-10-21 12:33:03 +0530

    Lives are updated

--- BananaHunt/Assets/Scripts/Managers/LivesManager.cs
+++ BananaHunt/Assets/Scripts/Managers/LivesManager.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class LivesManager : MonoBehaviour
 {
     public static LivesManager instance;
+    public float timeInMin;
     [SerializeField]
     TMP_Text lives;
+    [SerializeField]
+    TMP_Text timer;
     private int Lives;
+    private float remainingTime;
+    [SerializeField]
+    TMP_Text mainMenuLives;
+    [SerializeField]
+    TMP_Text mainMenuTimer;
+    
+    
     void Awake()
     {
         if(instance == null)
@@ -24,15 +35,46 @@ public class LivesManager : MonoBehaviour
         {
             Lives = PlayerPrefs.GetInt("Lives");
             lives.text = Lives.ToString();
+            mainMenuLives.text = Lives.ToString();
         }
-
+        if (PlayerPrefs.HasKey("RemainingTime"))
+        {
+            remainingTime = PlayerPrefs.GetFloat("RemainingTime");
+        }
+        else
+        {
+            remainingTime = timeInMin * 60;
+        }
+        CheckLives();
+        
     }
 
     void Update()
     {
-        if(Lives < 5 && Lives > 0)
+        if(Lives < 5)
         {
-            Debug.Log("Start Timer");
+            if (!timer.gameObject.activeSelf)
+            {
+                timer.gameObject.SetActive(true);
+            }
+            if (!mainMenuTimer.gameObject.activeSelf)
+            {
+                mainMenuTimer.gameObject.SetActive(true);
+            }
+            int minutes = Mathf.FloorToInt(remainingTime / 60f); // Get the total minutes
+            int seconds = Mathf.FloorToInt(remainingTime % 60f); // Get the remaining seconds
+
+            // Format the time as "MM:SS"
+            string formattedTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            timer.text = formattedTime;
+            mainMenuTimer.text = formattedTime;
+            remainingTime = remainingTime - Time.deltaTime;
+            if(remainingTime <= 0)
+            {
+                IncreaseLives();
+                remainingTime = timeInMin * 60;
+            }
         }
 
         
@@ -40,18 +82,54 @@ public class LivesManager : MonoBehaviour
         {
             Lives = 5;
             lives.text = Lives.ToString();
+            mainMenuLives.text = Lives.ToString();
+            if (timer.gameObject.activeSelf)
+            {
+                timer.gameObject.SetActive(false);
+                remainingTime = timeInMin * 60;
+            }
+            if (mainMenuTimer.gameObject.activeSelf)
+            {
+                mainMenuTimer.gameObject.SetActive(false);
+                remainingTime = timeInMin * 60;
+            }
         }
     }
     public void IncreaseLives()
     {
         Lives = Lives + 1;
         lives.text = Lives.ToString();
+        mainMenuLives.text = Lives.ToString();
     }
 
+    void CheckLives()
+    {
+        if (PlayerPrefs.HasKey("LastQuitTime"))
+        {
+            DateTime lastQuitTime = DateTime.Parse(PlayerPrefs.GetString("LastQuitTime"));
+            TimeSpan timeDifference = DateTime.Now - lastQuitTime;
+            int seconds = (int)timeDifference.TotalSeconds;
+            if (seconds < remainingTime)
+            {
+                remainingTime = remainingTime - seconds;
+            }
+            else
+            {
+                int totalMinutes = Mathf.FloorToInt(seconds / 300f); // Get the total minutes
+                Lives = Lives + totalMinutes;
+                lives.text = Lives.ToString();
+                mainMenuLives.text = Lives.ToString();
+                int totalSeconds = Mathf.FloorToInt(seconds % 300f); // Get the remaining seconds
+                remainingTime = totalSeconds;
+
+            }
+        }
+    }
     public void DecreaseLives()
     {
         Lives -= 1;
         lives.text = Lives.ToString();
+        mainMenuLives.text = Lives.ToString();
     }
 
     public int GetLives()
@@ -62,6 +140,8 @@ public class LivesManager : MonoBehaviour
     void OnApplicationQuit()
     {
         PlayerPrefs.SetInt("Lives", Lives);
+        PlayerPrefs.SetFloat("RemainingTime", remainingTime);
+        PlayerPrefs.SetString("LastQuitTime", DateTime.Now.ToString());
        
     }
 }
--- BananaHunt/Assets/Scripts/PlayerController.cs
+++ BananaHunt/Assets/Scripts/PlayerController.cs
@@ -6,24 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     Animator playerAnimations;
+    [SerializeField]
+    GameObject runningEffect;
     // Start is called before the first frame update
     void Start()
     {
-        
+        runningEffect.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
 
     {
+        
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
+            runningEffect.SetActive(false);
             StartCoroutine(JumpLeft());
 
+
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
+            runningEffect.SetActive(false);
             StartCoroutine(JumpRight());
         }
 
@@ -35,13 +41,17 @@ public class PlayerController : MonoBehaviour
         playerAnimations.SetBool("jump_left", true);
         yield return new WaitForSeconds(1);
         playerAnimations.SetBool("jump_left", false);
+        yield return new WaitForSeconds(0.35f);
+        runningEffect.SetActive(true);
     }
 
     IEnumerator JumpRight() {
         playerAnimations.SetBool("jump_right", true);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1f);
         transform.rotation = Quaternion.Euler(0f, 0f, 45f);
         playerAnimations.SetBool("jump_right", false);
+        yield return new WaitForSeconds(0.35f);
+        runningEffect.SetActive(true);
 
     }
 
